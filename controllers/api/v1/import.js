f1cad07898cc49d74bedc3aeffc0ddb870695a5e
drklrd <srvbhattarai@gmail.com>
2017-03-15 11:39:15 +0545

    include db creation within transaction

--- controllers/api/v1/import.js
+++ controllers/api/v1/import.js
@@ -1,5 +1,5 @@
 var data = require('../../../data/data.json');
-
+var dbInstance = require('../../../models');
 
 module.exports = {
 
@@ -39,108 +39,120 @@ module.exports = {
 
 		}
 
-		records
-			.create(recordsOptions)
-			.then(function(response) {
-				if (response && response.id) {
-
-					var recordId = response.id;
-
-					var houseStatusOptions = {
-						record_id: recordId,
-						status: record["g3/g3_a/house_status"],
-						cost: record["g3/g3_b/cost_1"] || record["g3/g3_c/cost_2"] || record["g3/g3_d/cost_3"],
-						house_design_followed: record["g3/g3_b/house_design_1"] || record["g3/g3_c/house_design_2"] || record["g3/g3_d/house_design_3"],
-						specify_house_design_followed: record["g3/g3_b/house_design_other_1"] || record["g3/g3_c/house_design_other_2"] || record["g3/g3_d/house_design_other_3"],
-						building_foundation: record["g3/g3_b/building_foundation_1"] || record["g3/g3_b/building_foundation_2"] || record["g3/g3_b/building_foundation_3"],
-						specify_building_foundation: record["g3/g3_b/building_foundation_other_1"] || record["g3/g3_b/building_foundation_other_2"] || record["g3/g3_b/building_foundation_other_3"],
-						roof_design: record["g3/g3_b/roof_design_1"] || record["g3/g3_c/roof_design_2"] || record["g3/g3_d/roof_design_3"],
-						specify_roof_design: record["g3/g3_b/roof_design_other_1"] || record["g3/g3_c/roof_design_other_2"] || record["g3/g3_d/roof_design_other_3"],
-						funding_source: record["g3/g3_b/funding_source_1"] || record["g3/g3_c/funding_source_2"] || record["g3/g3_c/funding_source_3"],
-						specify_funding_source: record["g3/g3_b/funding_source_other_1"] || record["g3/g3_c/funding_source_other_2"] || record["g3/g3_c/funding_source_other_3"],
-						building_code_followed: record["g3/g3_b/codes_followed_1"] || record["g3/g3_c/codes_followed_2"] || record["g3/g3_d/codes_followed_3"],
-						code_not_followed: record["g3/g3_b/code_not_followed_1"] || record["g3/g3_c/code_not_followed_2"] || record["g3/g3_d/code_not_followed_3"],
-						specify_why_not_followed: record["g3/g3_b/code_not_followed_other_1"] || record["g3/g3_c/code_not_followed_other_2"] || record["g3/g3_d/code_not_followed_other_3"]
-
-					};
+		var recordId;
 
 
 
-					house_status.create(houseStatusOptions)
-						.then(function(housestatusresponse) {
-							if (housestatusresponse && housestatusresponse.id) {
+		return dbInstance.sequelize.transaction(function(t) {
 
-								var grantOptions = {
-									record_id: recordId,
-									grant_received: record["g3/g3_e/received_grant"],
-									grant_spend_on: record["g3/g3_e/spent_grant"],
-									specify_grant_spend_on: record["g3/g3_e/spent_grant_other"],
-									reason_no_grant: record["g3/g3_e/reason_no_grant"],
-									specify_reason_no_grant: record["g3/g3_e/reason_no_grant_other"]
+				return records
+					.create(recordsOptions, {
+						transaction: t
+					})
+					.then(function(response) {
+						if (response && response.id) {
 
-								}
+							recordId = response.id;
 
-								grant_received.create(grantOptions)
-									.then(function(grantsresponse) {
-										if (grantsresponse && grantsresponse.id) {
-											
+							var houseStatusOptions = {
+								record_id: recordId,
+								status: record["g3/g3_a/house_status"],
+								cost: record["g3/g3_b/cost_1"] || record["g3/g3_c/cost_2"] || record["g3/g3_d/cost_3"],
+								house_design_followed: record["g3/g3_b/house_design_1"] || record["g3/g3_c/house_design_2"] || record["g3/g3_d/house_design_3"],
+								specify_house_design_followed: record["g3/g3_b/house_design_other_1"] || record["g3/g3_c/house_design_other_2"] || record["g3/g3_d/house_design_other_3"],
+								building_foundation: record["g3/g3_b/building_foundation_1"] || record["g3/g3_b/building_foundation_2"] || record["g3/g3_b/building_foundation_3"],
+								specify_building_foundation: record["g3/g3_b/building_foundation_other_1"] || record["g3/g3_b/building_foundation_other_2"] || record["g3/g3_b/building_foundation_other_3"],
+								roof_design: record["g3/g3_b/roof_design_1"] || record["g3/g3_c/roof_design_2"] || record["g3/g3_d/roof_design_3"],
+								specify_roof_design: record["g3/g3_b/roof_design_other_1"] || record["g3/g3_c/roof_design_other_2"] || record["g3/g3_d/roof_design_other_3"],
+								funding_source: record["g3/g3_b/funding_source_1"] || record["g3/g3_c/funding_source_2"] || record["g3/g3_c/funding_source_3"],
+								specify_funding_source: record["g3/g3_b/funding_source_other_1"] || record["g3/g3_c/funding_source_other_2"] || record["g3/g3_c/funding_source_other_3"],
+								building_code_followed: record["g3/g3_b/codes_followed_1"] || record["g3/g3_c/codes_followed_2"] || record["g3/g3_d/codes_followed_3"],
+								code_not_followed: record["g3/g3_b/code_not_followed_1"] || record["g3/g3_c/code_not_followed_2"] || record["g3/g3_d/code_not_followed_3"],
+								specify_why_not_followed: record["g3/g3_b/code_not_followed_other_1"] || record["g3/g3_c/code_not_followed_other_2"] || record["g3/g3_d/code_not_followed_other_3"]
 
-											var installmentOptions = {
-												record_id: recordId,
-												applied_for_second_installment : record["g3/g3_f/applied_sec_installment"],
-												how_long_since_applied : record["g3/g3_f/applied_how_long"],
-												why_not_applied : record["g3/g3_f/not_applied"],
-												specify_why_not_applied : record["g3/g3_f/not_applied_other"]
-											}
+							};
 
-											second_installment.create(installmentOptions)
-												.then(function(installmentreponse){
-													if(installmentreponse && installmentreponse.id){
-														res.json({
-															success: 1,
-															message: "Record created successfully !"
-														})
-													}else{
-														throw "Error creating installment"
-													}
-												})
-												.catch(function(err){
-													throw err;
-												})
+							return house_status.create(houseStatusOptions, {
+								transaction: t
+							});
 
 
+						} else {
 
-										} else {
-											throw "Error creating grants"
-										}
-									})
-									.catch(function(err) {
-										throw err;
-									})
+							throw "Error creating record";
 
-							} else {
-								throw "Error creating house status";
+						}
+					})
+					.then(function(housestatusresponse) {
+						if (housestatusresponse && housestatusresponse.id) {
+
+							var grantOptions = {
+								record_id: recordId,
+								grant_received: record["g3/g3_e/received_grant"],
+								grant_spend_on: record["g3/g3_e/spent_grant"],
+								specify_grant_spend_on: record["g3/g3_e/spent_grant_other"],
+								reason_no_grant: record["g3/g3_e/reason_no_grant"],
+								specify_reason_no_grant: record["g3/g3_e/reason_no_grant_other"]
+
 							}
-						})
-						.catch(function(err) {
-							throw err;
-						})
+
+							return grant_received.create(grantOptions, {
+								transaction: t
+							});
+
+						} else {
+							throw "Error creating house status";
+						}
+
+					})
+					.then(function(grantsresponse) {
+
+						if (grantsresponse && grantsresponse.id) {
+
+							var installmentOptions = {
+								record_id: recordId,
+								applied_for_second_installment: record["g3/g3_f/applied_sec_installment"],
+								how_long_since_applied: record["g3/g3_f/applied_how_long"],
+								why_not_applied: record["g3/g3_f/not_applied"],
+								specify_why_not_applied: record["g3/g3_f/not_applied_other"]
+							}
+
+							return second_installment.create(installmentOptions, {
+								transaction: t
+							});
+
+						} else {
+							throw "Error creating grants";
+						}
+
+					})
+
+				.then(function(installmentreponse) {
+					if (installmentreponse && installmentreponse.id) {
+						return true;
+					} else {
+						throw "Error creating installment"
+					}
+				})
 
 
-
-				} else {
-
-					throw "Error creating record";
-
-				}
 			})
-			.catch(function(err) {
+			.then(function(result) {
 				res.json({
-					success: 0,
-					error: 1,
-					message: err
+					success: 1,
+					message: "Record created successfully !"
 				})
 			})
+
+		.catch(function(err) {
+			res.json({
+				success: 0,
+				error: 1,
+				message: err
+			})
+		})
+
+
 
 	}
 
