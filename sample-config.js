53506dc24a69feda0efb9f634455477f26f4007a
drklrd <srvbhattarai@gmail.com>
2017-03-17 10:01:06 +0545

    add sample-config file

---
+++ sample-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+	versions: {
+		v1: '/api/v1'
+	},
+	codes: {
+
+		district: {
+
+			dolakha: '22',
+			sindhupalchowk: '23',
+			nuwakot: '28',
+			dhading: '30',
+			gorkha: '36',
+		},
+
+		status: {
+			construction_completed: '1',
+			construction_inprogress: '2',
+			construction_not_started: '3'
+		}
+	}
+}
