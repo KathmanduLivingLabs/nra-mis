e80b95e7e15f8d931658babc4a96659454ce6f6e
drklrd <srvbhattarai@gmail.com>
2017-03-22 14:44:03 +0545

    add basic cron setup

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// var cronJob = require('./cron');
+// cronJob.start();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -43,6 +45,7 @@ var versions = config.versions;
 for(var version in versions){
     require('./controllers'+ versions[version])(router);    
 }
+
 
 
 // catch 404 and forward to error handler
